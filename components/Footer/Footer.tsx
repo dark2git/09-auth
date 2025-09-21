ac32cf2976ccbc8ef15278bb100a23c56abc9fee
dark2git <fatalerror14.88@icloud.com>
2025-09-21 14:03:12 +0200

    contact info

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,20 @@
 import css from "./Footer.module.css";
 
 export default function Footer() {
-    return (
-        <footer className={css.footer}>
-            <div className={css.content}>
-                <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
-                <div className={css.wrap}>
-                    <p>Developer: your name</p>
-                    <p>
-                        Contact us:
-                        <a href="mailto:student@notehub.app">student@notehub.app</a>
-                    </p>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={css.footer}>
+      <div className={css.content}>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <div className={css.wrap}>
+          <p>Developer: Vadym Samardak</p>
+          <p>
+            Contact us:
+            <a href="mailto:vadym.samardak@dark2care.com">
+              vadym.samardak@dark2care.com
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
